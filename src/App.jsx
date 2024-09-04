899a530948199a4ec8442bba6944a7217741f20f
jorgeduje <jorgeduje4@gmail.com>
2024-09-04 20:11:33 -0300

    se implementa navbar y se trabaja con props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import Navbar from "./components/layouts/navbar/Navbar";
+import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
+
 function App() {
+  let saludo = "hola como estas";
   return (
     <div>
-      <h1>App</h1>
+      <Navbar />
+      <ItemListContainer greeting={saludo} y="2" maria="dasda" />
+      {/* <ItemListContainer {greeting:{saludo} y:"2" maria:"dasda"} />   */}
     </div>
   );
 }
 
 export default App;
+// props = {greeting:{saludo} y:"2" maria:"dasda"}
+// props.maria
---
+++ src/components/layouts/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import CartWidget from "../../common/cartWidget/CartWidget";
+import "./navbar.css";
+const Navbar = () => {
+  return (
+    <div className="container-nav">
+      {/* logo o el nombre de la empresa  */}
+      <h3>Comision-71835</h3>
+      {/* al medio un listado de categorias clickeables */}
+      <ul>
+        <li>Todas</li>
+        <li>Urbanas</li>
+        <li>Deportivas</li>
+      </ul>
+      <CartWidget />
+    </div>
+  );
+};
+
+export default Navbar;
