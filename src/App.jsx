be7350436c95bfd7bcbf59021f490f25aff89bdf
jorgeduje <jorgeduje4@gmail.com>
2024-09-04 18:26:03 -0300

    se limpia la rama main

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { Footer } from "./components/footer/Footer";
-import { Main } from "./components/main/Main";
-import { Navbar } from "./components/navbar/Navbar";
-
 function App() {
   return (
     <div>
-      <Navbar />
-      <Main />
-      <Footer />
+      <h1>App</h1>
     </div>
   );
 }
