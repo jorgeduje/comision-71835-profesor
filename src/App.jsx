92f32946ea2f036fdc6e689eb079959d44d66677
jorgeduje <jorgeduje4@gmail.com>
2024-09-30 20:58:48 -0300

    se trabaja con rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
+import CartContainer from "./components/pages/cart/CartContainer";
 import Navbar from "./components/layouts/navbar/Navbar";
 import ItemDetailContainer from "./components/pages/itemDetail/ItemDetailContainer";
-import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-      <ItemDetailContainer />
-    </div>
+      <Routes>
+        <Route path={"/"} element={<ItemListContainer />} />
+        <Route
+          path={"/category/:categoryName"}
+          element={<ItemListContainer />}
+        />
+
+        <Route path={"/cart"} element={<CartContainer />} />
+
+        <Route path={"/productDetail/:id"} element={<ItemDetailContainer />} />
+
+        <Route path="*" element={<h2>404 not found</h2>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
